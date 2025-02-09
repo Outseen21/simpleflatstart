87d0f719bb061243a8ad8424fa9c868d8ce0ff94
outseen21 <luviehama@gmail.com>
2025-02-10 00:07:58 +0100

    sadd

--- animated.js
+++ animated.js
@@ -1,11 +1,18 @@
-function showLoadingAnimation(event) {
-    event.preventDefault(); // Zatrzymanie domyślnego działania linku
-    const href = event.currentTarget.getAttribute('href');
+document.addEventListener("DOMContentLoaded", () => {
+    // Animacja uruchamia się, gdy strona jest w pełni załadowana
+    const fadeElements = document.querySelectorAll(".fade-in");
+    fadeElements.forEach((element) => {
+        element.style.visibility = "visible"; // Pokazuje element przed animacją
+        element.classList.add("visible");
+    });
+});
 
-    // Dodaj efekt zanikania przed przejściem
+function showLoadingAnimation(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+
     document.body.style.animation = "fadeOut 0.5s ease-in-out";
 
-    // Po zakończeniu animacji przenieś na nową stronę
     setTimeout(() => {
         window.location.href = href;
     }, 500);
