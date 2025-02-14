21e896eccb4adb86f3e44710fdda7e93087c4794
outseen21 <luviehama@gmail.com>
2025-02-14 09:15:05 +0100

    ewaea

---
+++ js/gallery.js
@@ -0,0 +1,33 @@
+let currentImageIndex = 0;
+const images = {
+  projekt1: [
+    "img/portfolio1_1.jpg",
+    "img/portfolio1_2.jpg",
+    "img/portfolio1_3.jpg",
+  ]
+};
+let currentProject = "";
+
+function openGallery(project) {
+  currentProject = project;
+  currentImageIndex = 0;
+  document.querySelector(`#${project}`).style.display = "flex";
+  updateGalleryImage();
+}
+
+function closeGallery() {
+  document.querySelector(`#${currentProject}`).style.display = "none";
+  currentProject = "";
+}
+
+function changeImage(direction) {
+  currentImageIndex =
+    (currentImageIndex + direction + images[currentProject].length) %
+    images[currentProject].length;
+  updateGalleryImage();
+}
+
+function updateGalleryImage() {
+  const modal = document.querySelector(`#${currentProject} .gallery-image`);
+  modal.src = images[currentProject][currentImageIndex];
+}
